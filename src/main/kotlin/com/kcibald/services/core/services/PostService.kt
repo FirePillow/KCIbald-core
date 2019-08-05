1cc2fa3cd9ffe5349c6a886653aa6c467fff9cd7
wusatosi <hi@wusatosi.com>
2019-08-05 23:58:01 +0800

    updated PostService interface

--- src/main/kotlin/com/kcibald/services/core/services/PostService.kt
+++ src/main/kotlin/com/kcibald/services/core/services/PostService.kt
@@ -1,14 +1,14 @@
 package com.kcibald.services.core.services
 
-import com.kcibald.services.core.QueryResult
+import com.kcibald.services.core.QueryResults
 import com.kcibald.services.core.proto.PostHead
 import com.kcibald.services.core.proto.PostInfo
 import com.kcibald.services.core.proto.QueryConfig
 
 interface PostService {
 
-    suspend fun getPostHeadsUnderRegion(regionId: String, queryConfig: QueryConfig): QueryResult<PostHead>
+    suspend fun getPostHeadsUnderRegion(regionId: String, queryConfig: QueryConfig): QueryResults<PostHead>?
 
-    suspend fun getPostsUnderRegion(regionId: String, queryConfig: QueryConfig): QueryResult<PostInfo>
+    suspend fun getPostsUnderRegion(regionId: String, queryConfig: QueryConfig): QueryResults<PostInfo>?
 
 }
